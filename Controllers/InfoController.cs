378cc1f839865b2051899fd7b9239549c32404d5
Yurii-ua74 <postffild@gmail.com>
2024-09-25 20:56:51 +0300

    anime_add

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rozetka.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
