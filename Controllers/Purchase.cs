bd97c5dbf87e381e0e2ae18d72af3fe9f9dc16db
Yurii-ua74 <postffild@gmail.com>
2024-10-05 12:14:15 +0300

    add_controllers

---
+++ Controllers/Purchase.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rozetka.Controllers
+{
+    public class Purchase : Controller
+    {
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Purchase/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Purchase/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Purchase/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Purchase/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Purchase/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Purchase/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Purchase/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
