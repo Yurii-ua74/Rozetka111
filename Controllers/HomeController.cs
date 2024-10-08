4ff674b7627633f33bff0aabf8af971d7eb46550
andrii-bilyi <100190822+andrii-bilyi@users.noreply.github.com>
2024-10-08 13:59:40 +0300

    voiceSearch

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,54 +43,7 @@ namespace Rozetka.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        // GET: /Home/GetSearchResult
-        public async Task<IActionResult> GetSearchResult(string inputSearching)
-        {
-            if (string.IsNullOrEmpty(inputSearching))  // Если поисковый запрос пуст или null, 
-            {
-                // Возвращаем пустую модель ProductSearchViewModel
-                return View(new ProductSearchViewModel());
-            }
-
-            if (inputSearching.Length < 2)
-            {
-                return View(new ProductSearchViewModel()); // Возвращаем пустую модель при коротком запросе
-            }
-
-            // Разбиваем запрос на слова
-            string[] words = inputSearching.Split(' ');
-
-            // Инициализируем модель поиска
-            ProductSearchViewModel productViewModel = new ProductSearchViewModel();
-
-            // Загружаем продукты из базы данных
-            var query = _context.Products
-                    .Include(p => p.ProductType)
-                    .Include(p => p.Brand)
-                    .Include(p => p.Childcategory)
-                    .Include(p => p.ProductImages)
-                    .Include(p => p.ProductColor)
-                    .Include(p => p.Reviews)
-                    .AsQueryable(); 
-
-            foreach (var word in words)
-            {
-                if (word.Length > 1)
-                {
-                         query = query.Where(product =>
-                            (product.Title != null && product.Title.ToLower().Contains(word.ToLower())) ||
-                            (product.ProductType != null && product.ProductType.Title.ToLower().Contains(word.ToLower())) ||
-                            (product.Brand != null && product.Brand.Title.ToLower().Contains(word.ToLower())));
-                }
-            }
-
-            // Выполняем запрос
-            productViewModel.SearchingResults = await query.ToListAsync();
-
-            return View(productViewModel);
-        }
-
+        
         // Відкриття сторінки адміна
         public IActionResult AdminPage()
         {
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -305,10 +305,13 @@ namespace Rozetka.Controllers
         [HttpGet]
         public IActionResult Search(string search)
         {
-            if (string.IsNullOrWhiteSpace(search))
+            if (string.IsNullOrWhiteSpace(search) || search.Length < 2)
             {
-                return View("Index"); // Повертаємо на основну сторінку, якщо запит порожній
-            }
+                // Якщо нічого не знайдено в усіх категоріях, виводимо повідомлення
+                ViewBag.Message = "Для пошуку потрібно не меньше двох символів.";
+                return View(new List<Product>()); // Повертаємо порожній список продуктів
+                //return View("Index"); // Повертаємо на основну сторінку, якщо запит порожній
+            }            
 
             // Спочатку шукаємо збіги в Childcategory
             var productsFromChildcategory = _context.Childcategories
@@ -365,6 +368,52 @@ namespace Rozetka.Controllers
             // Якщо нічого не знайдено в усіх категоріях, виводимо повідомлення
             ViewBag.Message = "Нічого не знайдено за вашим запитом.";
             return View(new List<Product>()); // Повертаємо порожній список продуктів
+        }
+        // GET: /Product/GetSearchResult
+        public async Task<IActionResult> GetSearchResult(string inputSearching)
+        {
+            if (string.IsNullOrEmpty(inputSearching))  // Если поисковый запрос пуст или null, 
+            {
+                // Возвращаем пустую модель ProductSearchViewModel
+                return View(new ProductSearchViewModel());
+            }
+
+            if (inputSearching.Length < 2)
+            {
+                return View(new ProductSearchViewModel()); // Возвращаем пустую модель при коротком запросе
+            }
+
+            // Разбиваем запрос на слова
+            string[] words = inputSearching.Split(' ');
+
+            // Инициализируем модель поиска
+            ProductSearchViewModel productViewModel = new ProductSearchViewModel();
+
+            // Загружаем продукты из базы данных
+            var query = _context.Products
+                    .Include(p => p.ProductType)
+                    .Include(p => p.Brand)
+                    .Include(p => p.Childcategory)
+                    .Include(p => p.ProductImages)
+                    .Include(p => p.ProductColor)
+                    .Include(p => p.Reviews)
+                    .AsQueryable();
+
+            foreach (var word in words)
+            {
+                if (word.Length > 1)
+                {
+                    query = query.Where(product =>
+                       (product.Title != null && product.Title.ToLower().Contains(word.ToLower())) ||
+                       (product.ProductType != null && product.ProductType.Title.ToLower().Contains(word.ToLower())) ||
+                       (product.Brand != null && product.Brand.Title.ToLower().Contains(word.ToLower())));
+                }
+            }
+
+            // Выполняем запрос
+            productViewModel.SearchingResults = await query.ToListAsync();
+
+            return View(productViewModel);
         }
     }
 }
