a49d6081759aa2525f3bb93032e49d530adce4e5
StuliiVladyslav <stulii.vladyslav@gmail.com>
2024-10-12 13:44:24 +0200

    Order completed page

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rozetka.Data;
+using Rozetka.Data.Entity;
+using Rozetka.Models.ViewModels.Cart;
+using System.Security.Claims;
+
+namespace Rozetka.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        public IActionResult SuccessOrder()
+        {
+            return View("~/Views/Order/SuccessOrder.cshtml"); // Укажите полный путь к представлению
+        }
+    }
+}
---
+++ Models/Order/CartIndexVM.cs
@@ -0,0 +1,17 @@
+﻿using Rozetka.Data.Entity;
+
+namespace Rozetka.Models.ViewModels.Order
+{
+    //Клас CartIndexVM є класом для передачі даних в представлення (View)
+    //сторінки кошика покупок. Він представляє модель даних,
+    //яку використовують для відображення інформації про товари в кошику,
+    //загальну вартість та можливі URL для повернення після взаємодії з кошиком.
+    public class OrderIndexVM
+    {
+        public IEnumerable<CartItem> CartItems { get; set; } = default!;
+
+        public double TotalPrice { get; set; }
+
+        public string? ReturnUrl { get; set; } = default!;
+    }
+}
