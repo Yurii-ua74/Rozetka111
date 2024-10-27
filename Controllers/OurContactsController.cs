3ad44ffc301518502282fd468584a4505c4eafb4
StuliiVladyslav <stulii.vladyslav@gmail.com>
2024-10-27 22:21:02 +0100

    OurContacts

---
+++ Controllers/OurContactsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Rozetka.Data;
+using Rozetka.Data.Entity;
+using Rozetka.Models.ViewModels.Cart;
+using System.Security.Claims;
+
+namespace Rozetka.Controllers
+{
+    public class OurContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/OurContacts.cshtml");
+        }
+    }
+}
+
+
+
