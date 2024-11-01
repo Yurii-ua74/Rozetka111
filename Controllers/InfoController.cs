4cfef21830cc663dc8057aee138b171231e7863b
StuliiVladyslav <stulii.vladyslav@gmail.com>
2024-11-01 21:14:58 +0100

    Footer completed

--- Controllers/InfoController.cs
+++ Controllers/InfoController.cs
@@ -17,5 +17,10 @@ namespace Rozetka.Controllers
         public IActionResult Paymentmethods() { return View(); }
         public IActionResult Delivery() { return View(); }
         public IActionResult OpenPickupPoint() { return View(); }
+        public IActionResult OfferWarehouse() { return View(); }
+        public IActionResult DeliverOrders() { return View(); }
+        public IActionResult CreditPage() { return View(); }
+        public IActionResult GuaranteePage() { return View(); }
+        public IActionResult RefundPage() { return View(); }
     }
 }
